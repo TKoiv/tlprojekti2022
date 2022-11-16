52f4d6710e8db608833862d088bc40fa33db0c85
Tauno Koivisto <c1kota00@students.oamk.fi>
2022-11-16 13:33:07 +0200

    Python koodeja

---
+++ python/client_mysql.py
@@ -0,0 +1,19 @@
+import credentials as cr
+import mysql.connector
+
+
+mydb = mysql.connector.connect(
+  host=cr.host,
+  user=cr.user,
+  password=cr.password,
+  database=cr.database
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM rawdata where groupid = 73")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
